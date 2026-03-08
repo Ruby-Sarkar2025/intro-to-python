576dfab23daa67bd1547877f04dd5fb96dd3d6da
Ruby-Sarkar2025 <sarkar.ruby248@gmail.com>
2026-03-08 14:52:43 -0400

    Python Lesson 2 Homework

--- lesson_2/homework.py
+++ lesson_2/homework.py
@@ -22,18 +22,18 @@ print(total_cost)
 
 average_speed = 60
 hours_available = 4
-total_distance = ??? # calculate here
-print(???)  # print the result
+total_distance = average_speed * hours_available  # calculate here
+print(total_distance)  # print the result
 
 # ---------------------------------------------------------------------
 # Exercise 2 - Pizza Slices
 # A pizza is cut into 8 equal slices. Calculate and print how many
 # slices each person will get if there are 4 people sharing the pizza.
 
-pizza_slices = ???
-people = ???
-slices_per_person = ???
-print(???)
+pizza_slices = 8
+people = 4
+slices_per_person = pizza_slices / people
+print(slices_per_person)
 
 # ---------------------------------------------------------------------
 # Exercise 3 - Shopping Discount
@@ -45,7 +45,10 @@ print(???)
 # print the result.
 # The formula to count the discounted price:
 # multiply the original price by the discount percentage and divide by 100.
-
+original_price = 80
+discount_percentage = 20
+final_price = original_price - (original_price * discount_percentage)/100
+print(final_price)
 
 # ---------------------------------------------------------------------
 # Exercise 4 - Temperature Conversion
@@ -54,7 +57,9 @@ print(???)
 # temperature and print the result.
 # To convert Celsis to Fahrenheit you need to multiply
 # the temperature in Celsius by 9/5 and add 32 to the result
-
+Celsius = 32
+Fahrenheit = Celsius * 9/5 + 32
+print(Fahrenheit)
 
 # ---------------------------------------------------------------------
 # Exercise 5 - Gardening
@@ -63,7 +68,10 @@ print(???)
 # and print the area of the flowerbed.
 # To calculate the area of a circle, multiply π (~3.141) with the square of
 # the circle's radius.
-
+pi = 3.141
+radius = 3.5
+flowerbed = pi * radius * radius
+print(flowerbed)
 
 # ---------------------------------------------------------------------
 # Exercise 6 - Convert Temperature
@@ -73,7 +81,8 @@ print(???)
 # Your task is to convert the float temperature to an integer
 # temperature for display.
 # As an example, if the temperature is 24.8ºC, you need to print 24.
-
+temperature = 24.8
+print(int(temperature))
 
 # ---------------------------------------------------------------------
 # Exercise 7 - Baking Cookies
@@ -81,7 +90,12 @@ print(???)
 # want to distribute them evenly into 5 cookies. Calculate and
 # print the number of chocolate chips in each cookie and the
 # remaining chips.
-
+chips = 17
+cookies = 5
+chips_per_cookie = chips / cookies
+remainder = chips % cookies
+print("chips per cookie = ", chips_per_cookie)
+print("remaining chips = ", remainder)
 
 # ---------------------------------------------------------------------
 # Exercise 8 - Fix the Code - Event total earnings
@@ -99,11 +113,12 @@ print(???)
 #
 # For the values provided we are expecting a total earning of 750000,
 # however the code is not working correctly. Can you fix it?
-1st_event_participants = 250
-2nd_event_participants = 500
-ticket_$ = 1000
 
-total_earnings = (1st_event_total + 2nd_event_total) * ticket_$
+first_event_participants = 250
+second_event_participants = 500
+ticket_money = 1000
+
+total_earnings = (first_event_participants + second_event_participants) * ticket_money
 print(total_earnings)
 
 
@@ -129,8 +144,8 @@ student_1_age = 15
 student_2_age = 17
 student_3_age = 13
 
-students_age_mean = student_1_age + student_2_age + student_3_age / 3
-print(students_mean_age)
+students_age_mean = float(student_1_age + student_2_age + student_3_age) / 3
+print(students_age_mean)
 
 # ---------------------------------------------------------------------
 # Challenge (OPTIONAL!): Separating Digits of a Number
@@ -152,6 +167,15 @@ print(students_mean_age)
 number = 1597
 
 digit_1 = number % 10
+number = number // 10
+
+digit_2 = number % 10
+number = number // 10
+
+digit_3 = number % 10
+number = number // 10
+
+digit_4 = number % 10
 number = number // 10
 
 # print(digit_1) # will print 7
